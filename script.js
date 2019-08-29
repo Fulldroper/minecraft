e408623eb2155584aae1a3c456c380e9539efb18
Fulldroper <40297614+Fulldroper@users.noreply.github.com>
2019-08-29 18:06:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ if (!req.favicon) {
 canvas.innerHTML='<img id="mon-img" with="50px" height="50px" src="'+req.favicon+'"></img>';
 canvas.innerHTML+='<div><p>Online: '+req.online+'</p>'+
 '<p>Motd: '+req.motd+'</p>'+
-'<p id=some1 ><a id="ip-addr" onclick=CopyToClipboard("ip-addr")>'+domen+":"+port+'</a> <a style=float:right;>'+req.players.now+'/'+req.players.max+'</a></p></div>';
+'<p style=margin-top:5px; ><a id="ip-addr" onclick=CopyToClipboard("ip-addr")>'+domen+":"+port+'</a> <a style=float:right;>'+req.players.now+'/'+req.players.max+'</a></p></div>';
 
 var mon_img= document.getElementById('mon-img')
 
